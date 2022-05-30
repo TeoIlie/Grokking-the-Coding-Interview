4524d93f705a68f764970c773f92ee2e7878520e
Teodor Ilie <teo.altum.quinque@gmail.com>
2022-05-30 10:56:11 -0400

    Add files via upload

---
+++ subsets.py
@@ -0,0 +1,25 @@
+
+import copy
+
+def find_subsets(nums):
+  subsets = [[]]
+  # start by adding the empty subset
+  for currentNumber in nums:
+    # we will take all existing subsets and insert the current number in them to create new subsets
+    n = len(subsets)
+    for i in range(n):
+      # create a new subset from the existing subset and insert the current element to it
+      set1 = copy.deepcopy(subsets[i]) # deep copy, rather than shallow copy
+      set1.append(currentNumber)
+      subsets.append(set1)
+
+  return subsets
+
+
+def main():
+
+  print("Here is the list of subsets: " + str(find_subsets([1, 3])))
+  print("Here is the list of subsets: " + str(find_subsets([1, 5, 3])))
+
+
+main()
